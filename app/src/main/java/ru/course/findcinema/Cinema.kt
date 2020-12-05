bad15eaae335bb5b3213b8ec239a689e9c992e72
Egor Chetvernin <e.chetvernin@napoleonit.ru>
2020-12-05 15:52:08 +0500

    Initial commit

---
+++ app/src/main/java/ru/course/findcinema/Cinema.kt
@@ -0,0 +1,5 @@
+package ru.course.findcinema
+
+import java.io.Serializable
+
+data class Cinema(val name: String, val year: String) : Serializable
---
+++ app/src/main/java/ru/course/findcinema/MainActivity.kt
@@ -0,0 +1,65 @@
+package ru.course.findcinema
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+        btnGoToDetails.setOnClickListener {
+            val cinema = Cinema("Шерлок Холмс 2", "2012")
+            val intent = Intent(this, CinemaDetailsActivity::class.java)
+            intent.putExtra("Cinema", cinema)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreInstanceState")
+    }
+
+}
